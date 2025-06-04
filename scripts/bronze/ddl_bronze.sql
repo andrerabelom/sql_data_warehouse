8dae4816f04c06d9a659c97a53e0e44bd6e29868
André Rabelo Moraes <andrerabelom@gmail.com>
2025-06-04 18:53:41 -0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -28,8 +28,8 @@ CREATE TABLE bronze.crm_cust_info(
 	cst_key NVARCHAR(20),
 	cst_firstname NVARCHAR(50),
 	cst_lastname NVARCHAR(50),
-	cst_marital_status NCHAR(5),  -- 'S' ou 'M'
-	cst_gndr NCHAR(1),            -- 'M' ou 'F'
+	cst_marital_status NVARCHAR(5),  -- 'S' ou 'M'
+	cst_gndr NVARCHAR(1),            -- 'M' ou 'F'
 	cst_create_date DATE
 )
 GO
@@ -41,21 +41,21 @@ CREATE TABLE bronze.crm_prd_info(
     prd_key NVARCHAR(30),
     prd_nm NVARCHAR(100),
     prd_cost DECIMAL(10,2),           -- Custo pode ser decimal, com 2 casas decimais
-    prd_line CHAR(5),                 -- Linha do produto (ex: 'R' ou 'S')
-    prd_start_dt DATE,
-    prd_end_dt DATE
+    prd_line NVARCHAR(5),                 -- Linha do produto (ex: 'R' ou 'S')
+    prd_start_dt DATETIME,
+    prd_end_dt DATETIME
 )
 GO
 CREATE TABLE bronze.crm_sales_details(
-    sls_ord_num NVARCHAR(20),  -- Número do pedido
-    sls_prd_key NVARCHAR(30),              -- Código do produto
-    sls_cust_id INT,                       -- ID do cliente
-    sls_order_dt DATE,                     -- Data do pedido
-    sls_ship_dt DATE,                      -- Data de envio
-    sls_due_dt DATE,                       -- Data de entrega prevista
-    sls_sales DECIMAL(10,2),               -- Valor total da venda
-    sls_quantity INT,                      -- Quantidade vendida
-    sls_price DECIMAL(10,2)                -- Preço unitário
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
 )
 
 GO
